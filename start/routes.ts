946d12ad768a12e4b0cd2dad6aaa3bec0548dc16
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-05-06 14:43:52 +0200

    feat(routes): implement initial routing with health check WAR-18

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+const HealthCheckController = () => import('#controllers/health_check_controller')
+const RootController = () => import('#controllers/root_controller')
+const ApiInfoController = () => import('#controllers/api_info_controller')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+router.get('/', [RootController])
+router.get('/health', [HealthCheckController])
+
+const apiV1 = router.group(() => {
+  router.get('/', [ApiInfoController])
 })
+
+apiV1.prefix('/api/v1')
